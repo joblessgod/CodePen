1cd56baa5b78ae97aca64bf63428a4be75a7a05f
JobLessGod <iamjoblessgod@gmail.com>
2024-07-06 23:20:58 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+      perspiciatis, et dolores dolorem maiores sequi voluptate ipsam a vero quia
+      quasi dolor placeat iusto deserunt? Similique doloribus eaque deserunt.
+      Beatae!
+    </>
+  );
+}
+
+export default App;
