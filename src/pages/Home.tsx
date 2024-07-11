f506fc8b34d6caccb08b2253178e20d4967cb1aa
JobLessGod <iamjoblessgod@gmail.com>
2024-07-11 16:51:03 +0545

    added simple home design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
-
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className=" w-full h-[calc(100dvh-60px)] text-white flex justify-center items-center flex-col gap-4">
+      <h1 className="text-6xl font-bold flex">
+        WD Compiler
+      </h1>
+      <p className="text-gray-500 text-center">
+        Compiler HTML, CSS JavaScript Code on the go and share it with your
+        friends
+      </p>
+    </div>
+  );
 }
