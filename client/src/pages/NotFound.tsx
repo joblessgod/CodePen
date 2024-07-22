16e9361456af4bcc67a70c7aa7936c257bbb094e
JobLessGod <iamjoblessgod@gmail.com>
2024-07-22 18:46:51 +0545

    Update NotFound.tsx

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <>
       <Header />
-      <div classNameName="w-full h-[calc(100dvh-60px)] bg-gray-900 text-white p-3 flex justify-center items-center text-2xl font-bold">
+      <div className="w-full h-[calc(100dvh-60px)] bg-gray-900 text-white p-3 flex justify-center items-center text-2xl font-bold">
         404 - Page nahi mila bhai 😜
       </div>
     </>
