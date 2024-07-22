ff5df79e22ee7217a325c5ddb58a2d0b5137d594
JobLessGod <iamjoblessgod@gmail.com>
2024-07-22 18:37:02 +0545

    404 page test

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,11 +1,34 @@
 import Header from "../components/Header";
+import "./NotFoundStyle.css";
 
 export default function NotFound() {
   return (
     <>
       <Header />
-      <div className="w-full h-[calc(100dvh-60px)] bg-gray-900 text-white p-3 flex justify-center items-center text-2xl font-bold">
+      {/* <div classNameName="w-full h-[calc(100dvh-60px)] bg-gray-900 text-white p-3 flex justify-center items-center text-2xl font-bold">
         404 - Page nahi mila bhai 😜
+      </div> */}
+      <div className="room !h-[calc(100dvh-60px)]">
+        <div className="cuboid">
+          <div className="side"></div>
+          <div className="side"></div>
+          <div className="side"></div>
+        </div>
+        <div className="oops">
+          <h2>OOPS!</h2>
+          <p>We can't find the page that you're looking for :(</p>
+        </div>
+        <div className="center-line">
+          <div className="hole">
+            <div className="ladder-shadow"></div>
+            <div className="ladder"></div>
+          </div>
+          <div className="four">4</div>
+          <div className="four">4</div>
+          <div className="btn">
+            <a href="/">BACK TO HOME</a>
+          </div>
+        </div>
       </div>
     </>
   );
