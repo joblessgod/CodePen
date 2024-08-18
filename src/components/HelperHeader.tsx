4dff40f60b2f35e430f5ce4ce47faea95adaf890
JobLessGod <156568930+joblessgod@users.noreply.github.com>
2024-08-18 23:11:05 +0545

    Update HelperHeader.tsx

--- src/components/HelperHeader.tsx
+++ src/components/HelperHeader.tsx
@@ -275,8 +275,8 @@ export default function HelperHeader() {
               <div className="flex items-center space-x-2">
                 <iframe
                   className="rounded-sm"
-{/*                   width="560" */}
-{/*                   height="315" */}
+                  width="560"
+                  height="315"
                   src="https://www.youtube.com/embed/GAXXlbUlxNg?si=0Q8oEB7Pr-wqCBNu"
                   title="LIVE Code Editor | YouTube"
                   allowFullScreen
